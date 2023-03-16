da61b6cbc20938fdbe81df9fdf9ad06090906693
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-15 21:22:36 -0300

    Create vetores_codigosIniciais3_strings.R

---
+++ vetores_codigosIniciais3_strings.R
@@ -0,0 +1,9 @@
+vetor3 <- c(TRUE, FALSE, 1, 2)
+print(vetor3) #printa o vetor
+print(vetor3[2]) #printa o segundo elemento do vetor (perceba que a contagem começa em 1, e não em 0)
+print(sort(vetor3)) #ordena o vetor e depois printa
+
+vetor3 <- c("TRUE", "FALSE", 1, 2) #Transforma tudo em string
+print(vetor3) #printa o vetor
+print(vetor3[2]) #printa o segundo elemento do vetor (perceba que a contagem começa em 1, e não em 0)
+print(sort(vetor3)) #ordena o vetor e depois printa
