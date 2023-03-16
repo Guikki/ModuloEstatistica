b6aaf0137ba058adcc85c187472ed0240f8ec1f4
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-15 21:20:09 -0300

    Create codigosIniciais2_comandoSort.R

---
+++ codigosIniciais2_comandoSort.R
@@ -0,0 +1,4 @@
+vetor2 <- c(2,5,20,15,10)
+print(vetor2) #printa o vetor
+print(vetor2[2]) #printa o segundo elemento do vetor (perceba que a contagem começa em 1, e não em 0)
+print(sort(vetor2)) #ordena o vetor e depois printa
