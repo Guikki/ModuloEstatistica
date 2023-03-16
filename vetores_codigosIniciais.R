b590408f8dfd851889bab4dd54a95cb013a7a9b8
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-15 21:15:45 -0300

    Create vetores_codigosIniciais.R

---
+++ vetores_codigosIniciais.R
@@ -0,0 +1,5 @@
+vetor1 <- c("R","Python","Java") 
+# print(vetor1) - printa o vetor todo
+# print(vetor1[1]) - printa só o primeiro elemento do vetor
+# print(vetor1[1:3]) - printa os elementos do 1 ao 3
+print(vetor1[1,3]) # printa apenas os elementos 1 e 3, ignorando os outros.
