a02561286d53771812dce214937571d82eb38059
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-15 21:35:13 -0300

    Create matriz_codigosIniciais.R

---
+++ matriz_codigosIniciais.R
@@ -0,0 +1,10 @@
+mat1 <- matrix( c(1,5,10,30,15,8), nrow=3, ncol=2, 
+byrow=TRUE) 
+print(mat1) 
+# Organiza com 3 linhas e 2 colunas
+
+
+mat2 <- matrix( c(1,5,10,30,15,8), nrow=2, ncol=3, 
+byrow=TRUE) 
+print(mat2) 
+# Organiza com 2 linhas e 3 colunas
